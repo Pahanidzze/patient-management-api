5796bbfd1c2974bda2d0cb21932e290578ff0f76
Pahanidzze <0pahan001@gmail.com>
2025-10-13 11:48:36 +0500

    schema dump

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_25_095227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "doctor_patients", force: :cascade do |t|
+    t.bigint "doctor_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id", "patient_id"], name: "index_doctor_patients_on_doctor_id_and_patient_id"
+    t.index ["doctor_id"], name: "index_doctor_patients_on_doctor_id"
+    t.index ["patient_id"], name: "index_doctor_patients_on_patient_id"
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "middle_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "middle_name"
+    t.date "birthday", null: false
+    t.string "gender", null: false
+    t.integer "height", null: false
+    t.decimal "weight", precision: 5, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["birthday"], name: "index_patients_on_birthday"
+    t.index ["first_name", "last_name", "middle_name", "birthday"], name: "idx_on_first_name_last_name_middle_name_birthday_3696b9b713"
+    t.index ["gender"], name: "index_patients_on_gender"
+  end
+
+  add_foreign_key "doctor_patients", "doctors"
+  add_foreign_key "doctor_patients", "patients"
 end
